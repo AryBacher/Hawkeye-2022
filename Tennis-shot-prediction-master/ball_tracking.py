ebf3da25df39d93bc8965d78561f3875f9cd6c33
GuidoZyl <guidozyl@gmail.com>
2022-08-05 11:08:41 -0300

    Minor changes

--- Tennis-shot-prediction-master/ball_tracking.py
+++ Tennis-shot-prediction-master/ball_tracking.py
@@ -94,11 +94,10 @@ while True:
 		center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
 
 		# only proceed if the radius meets a minimum size
-		if radius > 10:
+		if radius > 0:
 			# draw the circle and centroid on the frame,
 			# then update the list of tracked points
-			cv2.circle(frame, (int(x), int(y)), int(radius),
-				(0, 255, 255), 2)
+			cv2.circle(frame, (int(x), int(y)), int(radius), (0, 255, 255), 2)
 			cv2.circle(frame, center, 5, (0, 0, 255), -1)
 
 	# update the points queue
