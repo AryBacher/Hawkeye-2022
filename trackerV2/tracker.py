fe249a5d7e56bf37ee37d5bc2e1a27bb0ee401f9
GuidoZyl <guidozyl@gmail.com>
2022-08-08 12:09:17 -0300

    Minor changes

--- trackerV2/tracker.py
+++ trackerV2/tracker.py
@@ -1,11 +1,11 @@
-from cmath import rect
+
 import cv2 as cv
 
 SENSITIVITY_VALUE = 20
 BLUR_SIZE = 10
 theObject = {0, 0}
 
-objectBoundingRectangle = rect(0, 0, 0, 0)
+objectBoundingRectangle = (0, 0, 0, 0)
 
 def searchForMovement(thresholdImage, cameraFeed):
     objectDetected = False
