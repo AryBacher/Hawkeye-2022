5ad84ecbada376ab218526d6d6e60e4a2b707fa7
Alan-Yeger <101569295+Alan-Yeger@users.noreply.github.com>
2022-08-24 15:53:33 -0300

    SignUp page created

--- Front-End/client/src/pages/NotFoundPage.jsx
+++ Front-End/client/src/pages/NotFoundPage.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 function NotFoundPage() {
   return (
     <div>NotFoundPage</div>
+    
   )
 }
 
--- Front-End/client/src/pages/SignUpPage.jsx
+++ Front-End/client/src/pages/SignUpPage.jsx
@@ -1,24 +1,23 @@
 import React from "react";
 import "../stylesheets/SignUpPageStylesheets/SignUpPage.css";
+import BtnContinueGoogle from "../components/BtnContinueGoogle";
 
 function SignUpPage() {
   return (
     <div className="wrapper">
       <div className="left-side">
         <img src="#" alt="Aún no lo sé" />
-      </div>
-
-      <div className="right-side">
         <svg className="logo-icon"></svg>
         <h2 className="title">Registrarse</h2>
         <form action="#" method="post">
           <label for="username" />
           <input
             type="text"
-            id="username"
-            name="username"
-            placeholder="Nombre de usuario"
+            id="email"
+            name="email"
+            placeholder="Email"
           />
+        <BtnContinueGoogle routeName="/SignUp" textBtn="Únete gratis" />
 
           <label for="password" />
           <input
@@ -36,6 +35,16 @@ function SignUpPage() {
             placeholder="Confirmar contraseña"
           />
         </form>
+      </div>
+
+      <div className="right-side">
+      <h1 className="hero-title">
+            El ojo de halcón
+            <br />
+            en la palma de
+            <br />
+            tu mano.
+          </h1>
       </div>
     </div>
   );
