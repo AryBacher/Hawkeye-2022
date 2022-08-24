c7ba6f7ffe5dd02b7b64e61f180d77d27d1d2889
arybac <aryyeze@gmail.com>
2022-08-24 17:26:54 -0300

    Court Detector 5

--- Court_Detection/court_detector4.py
+++ Court_Detection/court_detector4.py
@@ -11,14 +11,28 @@ def display_lines(img, lines):
     cv2.imshow('Image with Lines',img)
     cv2.waitKey(0)
 
-img = cv2.imread('Foto Video3jpg.jpg')
+def filter_by_coordinates(lines, box):
+    xmin, ymin, xmax, ymax = box
+    lines_filtered = list()
+    for line in lines:
+        x1, y1, x2, y2 = line
+        if min(x1, x2) < xmin or min(y1,y2) < ymin or max(x1,x2) > xmax or max(y1,y2) > ymax:
+            pass
+        else:
+            lines_filtered.append(line)
+    return lines_filtered
+
+img = cv2.imread('tennis2.png')
 img = cv2.resize(img, (img.shape[1] // 1, img.shape[0] // 1))
+#img = imutils.resize(img, width=800)
 
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 gray = cv2.threshold(gray, 180, 255, cv2.THRESH_BINARY)[1]
 
 lines = cv2.HoughLinesP(gray, 1, np.pi /180, 50, minLineLength=80, maxLineGap=20)
 lines = np.squeeze(lines)
+
+lines = filter_by_coordinates(lines, (100, 100, gray.shape[1] - 50, gray.shape[0] - 50))
 
 display_lines(img, lines)
 
---
+++ Court_Detection/court_detector5.py
@@ -0,0 +1,84 @@
+from imutils.video import VideoStream
+import numpy as np
+import argparse
+import cv2
+import time
+import math
+
+
+# construct the argument parse and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-v", "--video",
+	help="path to the (optional) video file")
+ap.add_argument("-b", "--buffer", type=int, default=64,
+	help="max buffer size")
+args = vars(ap.parse_args())
+
+def display_lines(frame, lines):
+    for line in lines:
+        x1, y1, x2, y2 = line
+
+        if x2 - x1 < 2 or y2 - y1 < 2:
+            continue
+        
+        cv2.line(frame, (x1,y1), (x2,y2), (0,255,0), 2)
+        cv2.circle(frame, (x1,y1), 1, (255,0,0), 2)
+        cv2.circle(frame, (x2,y2), 1, (255,0,0), 2)
+
+        cv2.imshow("Court Detector 5", frame)
+
+def filter_by_coordinates(lines, box):
+    xmin, ymin, xmax, ymax = box
+    lines_filtered = list()
+    for line in lines:
+        x1, y1, x2, y2 = line
+        if min(x1, x2) < xmin or min(y1,y2) < ymin or max(x1,x2) > xmax or max(y1,y2) > ymax:
+            pass
+        else:
+            lines_filtered.append(line)
+    return lines_filtered
+
+
+# if a video path was not supplied, grab the reference
+# to the webcam
+if not args.get("video", False):
+	vs = VideoStream(src=0).start()
+
+# otherwise, grab a reference to the video file
+else:
+	vs = cv2.VideoCapture(args["video"])
+
+time.sleep(2.0)
+
+while True:
+    frame = vs.read()
+    
+    frame = frame[1] if args.get("video", False) else frame
+    
+    if frame is None:
+        break
+
+    frame = cv2.resize(frame, (frame.shape[1] // 1, frame.shape[0] // 1))
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gray = cv2.threshold(gray, 180, 255, cv2.THRESH_BINARY)[1]
+
+    lines = cv2.HoughLinesP(gray, 1, np.pi /180, 50, minLineLength=80, maxLineGap=20)
+    lines = np.squeeze(lines)
+
+    lines = filter_by_coordinates(lines, (100, 100, gray.shape[1] - 50, gray.shape[0] - 50))
+
+    display_lines(frame, lines)
+
+    key = cv2.waitKey(1) & 0xFF
+
+    if key == ord("q"):
+        break
+
+if not args.get("video", False):
+    vs.stop()
+
+else:
+    vs.release()
+
+cv2.destroyAllWindows()
