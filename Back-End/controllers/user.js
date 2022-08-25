3f7b7572fdbb1c4e0745392806eabfbb0e58730f
Alan-Yeger <101569295+Alan-Yeger@users.noreply.github.com>
2022-08-25 09:12:23 -0300

    Initial structure define

---
+++ Back-End/controllers/user.js
@@ -0,0 +1,7 @@
+export const signUp = (req, res) =>{
+    return("Hola");
+}
+
+export const logIn = (req, res) =>{
+    return("Chau");
+}
--- Back-End/server/routes/user.routes.js
+++ Back-End/server/routes/user.routes.js
@@ -1,10 +1,17 @@
 //import { connection } from "../database.js";
 import { Router } from 'express'
+import { signUp, logIn } from "../../controllers/user.js";
+
 
 const router = Router();
 
 router.get('/images', (req, res) => {
     res.json({ "Hola": "Hola"})
 })
+
+router.post('/SignUp', signUp)
+
+router.post('/LogIn',logIn)
+
 
 export default router
