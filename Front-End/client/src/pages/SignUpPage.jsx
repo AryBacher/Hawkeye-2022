8428e7a88c97025d6d36ae4ee926b8f8759d3737
Alan-Yeger <101569295+Alan-Yeger@users.noreply.github.com>
2022-08-29 10:55:55 -0300

    Rebooting Front

--- Front-End/client/src/pages/SignUpPage.jsx
+++ Front-End/client/src/pages/SignUpPage.jsx
@@ -1,50 +1,22 @@
 import React from "react";
 import "../stylesheets/SignUpPageStylesheets/SignUpPage.css";
-import BtnContinueGoogle from "../components/BtnContinueGoogle";
+import FooterLogo from "../components/FooterLogo";
+import Form from "../components/Form";
 
 function SignUpPage() {
   return (
     <div className="wrapper">
       <div className="left-side">
-        <img src="#" alt="Aún no lo sé" />
-        <svg className="logo-icon"></svg>
-        <h2 className="title">Registrarse</h2>
-        <form action="#" method="post">
-          <label for="username" />
-          <input
-            type="text"
-            id="email"
-            name="email"
-            placeholder="Email"
-          />
-        <BtnContinueGoogle routeName="/SignUp" textBtn="Únete gratis" />
-
-          <label for="password" />
-          <input
-            type="password"
-            id="password"
-            name="password"
-            placeholder="Contraseña"
-          />
-
-          <label for="confirmPassword" />
-          <input
-            type="password"
-            id="confirmPassword"
-            name="confirmPassword"
-            placeholder="Confirmar contraseña"
-          />
-        </form>
+        <Form/>
+        <FooterLogo/>
       </div>
 
       <div className="right-side">
-      <h1 className="hero-title">
-            El ojo de halcón
+        <h1 className="hero-phrase">
+            El ojo de halcón en la
             <br />
-            en la palma de
-            <br />
-            tu mano.
-          </h1>
+            palma de tu mano.
+        </h1>
       </div>
     </div>
   );
