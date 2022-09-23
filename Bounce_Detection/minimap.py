0800b4c1802c201c48e32ed451654d91cd904116
GuidoZyl <91797603+GuidoZyl@users.noreply.github.com>
2022-09-23 09:40:04 -0300

    Continued minimap

--- Bounce_Detection/minimap.py
+++ Bounce_Detection/minimap.py
@@ -1,22 +1,17 @@
 import cv2
 import numpy as np
 
-#def minimap(pts):
-    # Se crea el minimapa
-minimap = np.zeros((524, 268, 3), np.uint8)
+def minimap(pts):
+    # Se abre el minimapa
+    minimap = cv2.imread("minimap.jpg")
 
-# Se dibujan las lineas de la cancha
-minimap = cv2.line(minimap, (25, 25), (243, 25), (255, 255, 255), 2)
-minimap = cv2.line(minimap, (25, 25), (25, 499), (255, 255, 255), 2)
-minimap = cv2.line(minimap, (25, 499), (243, 499), (255, 255, 255), 2)
-minimap = cv2.line(minimap, (243, 25), (243, 499), (255, 255, 255), 2)
-minimap = cv2.line(minimap, (25, 262), (243, 262), (255, 255, 255), 2)
+    # Se dibujan los puntos de pique
+    for pt in pts:
+        minimap = cv2.circle(minimap, pt, 3, (0, 0, 255), -1)
 
-#for pt in pts:
-#    minimap = cv2.circle(minimap, pt, 1, (0, 0, 255), 1)
-while True:
-    cv2.imshow("Minimapa", minimap)
-    
-    key = cv2.waitKey(1) & 0xFF
-    if key == ord("q"):
-        break
+    while True:
+        cv2.imshow("Minimapa", minimap)
+        
+        key = cv2.waitKey(1) & 0xFF
+        if key == ord("q"):
+            break
