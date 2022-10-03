231df342c2ccb34321dbc0297291ab66cba85eef
Alan-Yeger <101569295+Alan-Yeger@users.noreply.github.com>
2022-10-03 11:13:41 -0300

    Added promises to the connection

--- Back-End/server/database.js
+++ Back-End/server/database.js
@@ -1,18 +1,26 @@
-import mysql from 'mysql2';
+import mysql from 'mysql2/promise';
 
-export const connection = mysql.createConnection({
+export const pool = mysql.createPool({
     host: process.env.DB_HOST,
     user: 'root',
     password: 'rootroot',
     database: process.env.DB_DATABASE,
 });
  
-connection.connect((err) => {
+/*connection.connect((err) => {
     if(err){
         console.log("El error de conexión es: " + err);
         return;
     }
     console.log("Tamos conectados a la base de datos");
+})*/
+
+pool.getConnection()
+.then(() => {
+    console.log("Tamos conectados a la base de datos")
+})
+.catch((err) =>{
+    console.log("No tamos conectados por el error:" +err)
 })
 
 //module.export(connection);
