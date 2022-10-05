c278d53608b0ba5d28cfa2a70d113f729efb10a0
ArielAlzogarayFlores <102029851+ArielAlzogarayFlores@users.noreply.github.com>
2022-10-05 10:23:37 -0300

    Google Button Finished & Sign Up/Log In bugs fixed

--- Front-End-Vite/client/src/components/BtnGoogle.jsx
+++ Front-End-Vite/client/src/components/BtnGoogle.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import "../stylesheets/BtnGoogleStylesheets/BtnGoogle.css";
+import google from '../assets/img/google.png';
 
 function BtnGoogle() {
   return (
     <>
-      <button className="google">
-        <div id="google-icon"></div>
-        Continuar con Google
+      <button className="google-button">
+        <div id="icon-container">
+          <img src={google} alt="Logotipo de Google" />
+        </div>
+        <span>Continuar con Google</span>
       </button>
     </>
   );
