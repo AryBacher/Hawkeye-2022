dfc03922fc3b5d1fed45dcce27b7e4d95d0e79d3
Alan-Yeger <alanyeger1@gmail.com>
2022-10-05 10:27:27 -0300

    set up multer

--- Back-End/server/database.js
+++ Back-End/server/database.js
@@ -2,9 +2,9 @@ import mysql from 'mysql2/promise';
 
 export const pool = mysql.createPool({
     host: process.env.DB_HOST,
-    user: 'root',
-    password: 'rootroot',
-    database: 'dbhawkeye',
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE
 });
 
 pool.getConnection()
--- Back-End/server/index.js
+++ Back-End/server/index.js
@@ -1,7 +1,9 @@
 import express from "express";
 import 'dotenv/config';
 import cors from 'cors';
-import multer from 'multer';
+import multer, { diskStorage } from 'multer';
+import path from 'path';
+import { v4 as uuidv4 } from 'uuid';
 import UserRoutes from './routes/user.routes.js';
 import VideoRoutes from './routes/videos.routes.js';
 
@@ -12,15 +14,32 @@ app.use(express.json())
 app.use(cors())
 
 
+const storage = multer.diskStorage({
+    destination: ('./videos'),
+    filename: (req, file, cb) => {
+      cb(null, uuidv4() + path.extname(file.originalname).toLocaleLowerCase());
+    }
+    });
+
 app.use(multer({
+    storage,
     dest: './videos',
     limits: {fileSize: 100000000000},
+    fileFilter: (req, file, cb) => {
+        const filetypes = /mp4/
+        const mimetype = filetypes.test(file.mimetype);
+        const extname = filetypes.test(path.extname(file.originalname));
+        if (extname && mimetype){
+            return cb(null, true);
+        }
+        cb('Error: tipo de archivo no válido');
+    }
 }).single('video'))
 
 app.use(UserRoutes)
 app.use(VideoRoutes)
 
-app.set ('port', 4000);
+app.set ('port', process.env.PORT);
 
 app.listen (app.get('port'));
 
