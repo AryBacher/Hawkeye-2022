abc6a9f764dad529f8545c864d350d22ce352d73
Alan-Yeger <alanyeger1@gmail.com>
2022-10-05 09:29:48 -0300

    finished video queries

--- Back-End/server/routes/videos.routes.js
+++ Back-End/server/routes/videos.routes.js
@@ -1,13 +1,15 @@
 //import { connection } from "../database.js";
 import { Router } from 'express'
-import { filterVideo, uploadVideo } from "../controllers/videos.js";
+import { filterVideo, uploadVideo, deleteVideo, updateVideo } from "../controllers/videos.js";
 
 const router = Router();
 
-//router.post('/videos/upload', upload.array)
-
 router.post('/filterVideo', filterVideo)
 
-router.post('/UploadVideo', uploadVideo)
+router.post('/uploadVideo', uploadVideo)
+
+router.post('/deleteVideo', deleteVideo)
+
+router.post('/updateVideo', updateVideo)
 
 export default router
