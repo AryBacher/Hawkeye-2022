6634d01af77e8eb55a59b77662c014649bdb71b2
ArielAlzogarayFlores <102029851+ArielAlzogarayFlores@users.noreply.github.com>
2022-10-21 06:57:30 -0300

    Fifth attemp to make the multi-step form works
    - Finally I decided to make a single step form but soon I'll try to make it multi-step.
    - The validation schema is missing.

--- Front-End-Vite/client/src/pages/RecordPage.jsx
+++ Front-End-Vite/client/src/pages/RecordPage.jsx
@@ -1,39 +1,16 @@
 import React from 'react'
 import '../stylesheets/RecordPageStylesheets/RecordPage.css';
 import EndUseNavbar from '../components/EndUseNavbar';
-import { TextField, Button, Step, Stepper, StepLabel} from "@mui/material";
+import { TextField, Button, Step, Stepper, StepLabel, capitalize} from "@mui/material";
 import { fontWeight } from '@mui/system';
 import TypeField from '../components/Form UploadVideo Components/TypeField';
 import CheckboxOutstanding from '../components/Form UploadVideo Components/CheckboxOutstanding';
 import { Form, Formik, Field, useFormik, useFormikContext} from "formik";
 import { object, string, ref, date, mixed} from "yup";
-import {useState, Fragment} from 'react';
+import {useState, Fragment, useEffect} from 'react';
 import axios from 'axios';
-import InStep from '../components/FormUploadVideoPro';
 
 function RecordPage() {
-
-  // Los diferentes labels de los pasos del formulario, haremos un mapeo para recorrerlo y setearlos como título de cada paso correspondiente.
-
-  const steps = ['Datos del análisis', 'Video a analizar', 'Selecciona las esquinas'];
-
-  //Formulario por pasos cambios de estado y handleNext/handleBack/handleReset function
-
-  const [activeStep, setActiveStep] = useState(0);
-
-  const handleNext = ()=>{
-    setActiveStep((prevActiveStep) => prevActiveStep + 1);
-  }
-
-  const handleBack = ()=>{
-    setActiveStep((prevActiveStep) => prevActiveStep - 1);
-  }
-
-  const handleReset = () => {
-    setActiveStep(0);
-  };
-
-  //Función para que dependiendo el paso devuelva los inputs correspondientes.
 
 
   //Valores iniciales de los campos para colocar en el Formik "initialValues"
@@ -75,9 +52,227 @@ function RecordPage() {
               console.log(values);
             }}
           >
+            {({ setFieldValue, handleSubmit, errors, isValid, touched, dirty }) => (
+              <Form onSubmit={handleSubmit}>
+                <Field
+                  name="title"
+                  type="text"
+                  as={TextField}
+                  variant="outlined"
+                  color="primary"
+                  label="Título del análisis"
+                  size="normal"
+                />
+                <TypeField name="type" label="Tipo de análisis" />
+                <Field
+                  name="rival"
+                  type="name"
+                  as={TextField}
+                  variant="outlined"
+                  color="primary"
+                  label="Rival (opcional)"
+                  size="normal"
+                />
+                <Field
+                  name="date"
+                  type="date"
+                  as={TextField}
+                  variant="outlined"
+                  InputLabelProps={{
+                    shrink: true,
+                  }}
+                  color="primary"
+                  label="Fecha del evento"
+                  size="normal"
+                />
+                <input
+                  name="file"
+                  type="file"
+                  accept="video/*"
+                  onChange={(e) => {
+                    setFieldValue("file", e.currentTarget.files[0]);
+                  }}
+                />
+                <Field
+                  name="corners"
+                  type="text"
+                  as={TextField}
+                  variant="outlined"
+                  color="primary"
+                  label="Esquinas de la cancha"
+                  size="normal"
+                />
+                <Button
+                  variant="contained"
+                  type="submit"
+                  size="large"
+                  disabled={!dirty || !isValid}
+                  sx={{
+                    fontWeight: 600,
+                    textTransform: "capitalize",
+                    fontSize: "1em",
+                    height: "50px",
+                    padding: 0,
+                    borderRadius: "10px",
+                  }}
+                >
+                  Continuar
+                </Button>
+              </Form>
+            )}
+          </Formik>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default RecordPage;
+
+
+/* 
+
+Problemas:
+
+- Uso un switch para mostrar el contenido dependiendo del paso activo pero este esta por fuera del formik tag lo que me impide de usar el setFieldValue para con el input file.
+- No se marca el button de next como tipo submit aunque yo le ponga la condición para que así sea en el último paso.
+
+Código del form multi-step:
+
+  // Los diferentes labels de los pasos del formulario, haremos un mapeo para recorrerlo y setearlos como título de cada paso correspondiente.
+
+  const steps = ['Datos del análisis', 'Video a analizar', 'Selecciona las esquinas'];
+
+  //Formulario por pasos cambios de estado y handleNext/handleBack/handleReset function
+
+  const [activeStep, setActiveStep] = useState(0);
+
+  const handleNext = ()=>{
+    setActiveStep((prevActiveStep) => prevActiveStep + 1);
+  }
+
+  const handleBack = ()=>{
+    setActiveStep((prevActiveStep) => prevActiveStep - 1);
+  }
+
+  const handleReset = () => {
+    setActiveStep(0);
+  };
+
+  //Función para mostrar los diferentes campos dependiendo del paso activo.
+
+  const InStep = (activeStep) =>{
+
+    switch(activeStep){
+      
+      case 0 : return (
+      <>
+        <Field
+          name="title"
+          type="name"
+          as={TextField}
+          variant="outlined"
+          color="primary"
+          label="Título"
+          size="normal"
+        />
+        <TypeField name="type" label="Tipo de análisis" />
+        <Field
+          name="rival"
+          type="name"
+          as={TextField}
+          variant="outlined"
+          color="primary"
+          label="Rival (opcional)"
+          size="normal"
+        />
+        <Field
+          name="date"
+          type="date"
+          as={TextField}
+          variant="outlined"
+          InputLabelProps={{
+            shrink: true,
+          }}
+          color="primary"
+          label="Fecha del evento"
+          size="normal"
+        />
+      </>
+    );
+    case 2: return (
+      <>
+        <Field
+          name="corners"
+          type="text"
+          as={TextField}
+          variant="outlined"
+          color="primary"
+          label="Esquinas de la cancha"
+          size="normal"
+        />
+      </>
+    );
+    case 1: return (
+      <>
+        <input
+          name="file"
+          type="file"
+          accept="video/*"
+          onChange={(e)=>{setFieldValue("file", e.currentTarget.files[0])}}
+        />
+      </>
+    )}
+  }
+
+  //Valores iniciales de los campos para colocar en el Formik "initialValues"
+
+  const initialValues = {
+    title: '',
+    type: '',
+    rival: '',
+    date: '',
+    file: '',
+    corners:''
+  }
+
+  //Función para fetchear los valores finales del formulario
+
+  const useAxios = async (finalValues)=>{
+    const response = await axios.post('http://localhost:4000/UploadVideo', JSON.stringify(finalValues), {
+      headers: { 'Content-Type': 'application/json' },
+      withCredentials: true,
+    })
+    console.log(response)
+  }
+
+  //Ver el estado del paso activo tras cada cambio en su estado.
+
+  useEffect(()=>{
+    console.log(activeStep);
+  })
+
+  return (
+    <>
+      <div className="wrapper-r">
+        <EndUseNavbar grabarId="grabar" análisisId="" ayudaId="" />
+        <div className="form-header">
+          <div className="form-header-content">
+            <p>Formulario por pasos</p>
+            <h1>Sube un nuevo análisis</h1>
+          </div>
+        </div>
+        <div className="form-container">
+          <Formik
+            initialValues={initialValues}
+            onSubmit={(values) => {
+              useAxios(values);
+              console.log(values);
+            }}
+          >
             <Form autoComplete="off">
               <Stepper activeStep={activeStep} alternativeLabel>
-                {steps.map((label, index) => {
+                {steps.map((label) => {
                   const stepProps = {};
                   const labelProps = {};
                   return (
@@ -94,13 +289,13 @@ function RecordPage() {
                 </Fragment>
               ) : (
                 <Fragment>
-                  <InStep activeStep={activeStep}/>
+                  {InStep(activeStep)}
                   <Button disabled={activeStep === 0} onClick={handleBack}>
                     Volver
                   </Button>
-                  <Button onClick={handleNext} type={activeStep == steps.length - 1 ? 'submit' : 'button'}>
+                  <Button onClick={handleNext} type= {activeStep === steps.length -1 ? 'submit' : 'button'}>
                     {activeStep === steps.length - 1
-                      ? "Subir nuevo análisis"
+                      ? "Subir análisis"
                       : "Siguiente"}
                   </Button>
                 </Fragment>
@@ -111,89 +306,7 @@ function RecordPage() {
       </div>
     </>
   );
-}
-
-export default RecordPage;
 
 
-/*
-
-{inStep(activeStep)}
-
-<input id="file" name="file" type="file" onChange={(event) => {
-  setFieldValue("file", event.currentTarget.files[0]);
-}} />
-
-*/
-
-/*
-
-                  <div>
-                    <Field
-                      name='title'
-                      type='name'
-                      as={TextField}
-                      variant="outlined"
-                      color="primary"
-                      label="Título"
-                      size="normal"
-                    />
-                    <TypeField
-                      name="type"
-                      label="Tipo de análisis"
-                    />
-                    <Field
-                      name='rival'
-                      type='name'
-                      as={TextField}
-                      variant="outlined"
-                      color="primary"
-                      label="Rival (opcional)"
-                      size="normal"
-                    />
-                    <Field
-                      name='date'
-                      type='date'
-                      as={TextField}
-                      variant="outlined"
-                      InputLabelProps = {{
-                        shrink: true
-                      }}
-                      color="primary"
-                      label="Fecha del evento"
-                      size="normal"
-                    />
-                  </div>
-                  <div>
-                    <input 
-                      name='file' 
-                      type="file" 
-                      accept='video/*' 
-                      onChange={(event) => { setFieldValue("file", event.currentTarget.files[0]); }}
-                    />
-                  </div>
-                  <div>
-                    <Field
-                      name='corners'
-                      type='text'
-                      as={TextField}
-                      variant="outlined"
-                      color="primary"
-                      label="Esquinas de la cancha"
-                      size="normal"
-                    />
-                    <CheckboxOutstanding
-                      name="outstanding"
-                      label="Marcar como análisis destacado"
-                    />
-                  </div>
-                  <Button 
-                    variant="contained"
-                    type="submit"
-                    size="large"
-                    sx={{height: '50px', fontWeight: 600, textTransform: 'capitalize', borderRadius: '10px'}}
-                  >
-                    Subir análisis
-                  </Button>
 
 */
