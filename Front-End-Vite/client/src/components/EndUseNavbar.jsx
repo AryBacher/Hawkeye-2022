26ba8a1d691c884f3e50a15145b3db727e607a07
ArielAlzogarayFlores <102029851+ArielAlzogarayFlores@users.noreply.github.com>
2022-10-24 23:45:34 -0300

    click outside close logout pop-up feature added

--- Front-End-Vite/client/src/components/EndUseNavbar.jsx
+++ Front-End-Vite/client/src/components/EndUseNavbar.jsx
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import  LogoSmall from './LogoSmall';
 import Avatar from '@mui/material/Avatar';
-import {useState, useEffect} from 'react';
+import {useState, useEffec, useRef} from 'react';
 import '../stylesheets/EndUseNavbarStylesheets/EndUseNavbar.css';
 import { useParams } from "react-router-dom"
 
@@ -24,6 +24,24 @@ function EndUseNavbar({grabarId, análisisId, ayudaId}) {
   //   document.addEventListener('click', closeDropdown);
   // });
 
+  //Referencia al log out pop-up
+
+  const logOutBtn = useRef(null);
+  const profileBtn = useRef(null);
+
+  useEffect(()=>{
+
+    const clickOutside = (e)=>{
+      if(logOutBtn.current && !logOutBtn.current.contains(e.target) && !profileBtn.current.contains(e.target)){
+        setDropdown(false);
+      }
+    }
+
+    window.document.addEventListener('mousedown', clickOutside);
+    return ()=>{window.document.removeEventListener('mousedown', clickOutside)}
+
+  },[logOutBtn])
+
   return (
     <>
       <nav className='enduse-nav'>
@@ -41,7 +59,7 @@ function EndUseNavbar({grabarId, análisisId, ayudaId}) {
             </li>
           </ul>
 
-          <div className='profile-container' onClick={changeDropdown}>
+          <div className='profile-container' onClick={changeDropdown} ref={profileBtn}>
             <Avatar alt="Nico" sx={{ width: 36, height: 36 ,color: '#FCFCFC', backgroundColor: '#BF360C', marginLeft : '15px', fontSize: '16px' }}>N</Avatar>
             <p id='profile-name'>Nico</p>
             <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -51,7 +69,7 @@ function EndUseNavbar({grabarId, análisisId, ayudaId}) {
         </div>
       </nav>
 
-      <a href="/LogIn">
+      <a href="/LogIn" ref={logOutBtn}>
         <div className={dropdown? 'menu': 'menu notdropdown'}>
           <svg className="icon-logout" width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M11 13V15C11 15.5304 10.8244 16.0391 10.5118 16.4142C10.1993 16.7893 9.77536 17 9.33333 17H2.66667C2.22464 17 1.80072 16.7893 1.48816 16.4142C1.17559 16.0391 1 15.5304 1 15V3C1 2.46957 1.17559 1.96086 1.48816 1.58579C1.80072 1.21071 2.22464 1 2.66667 1H9C9.92042 1 11 1.8955 11 3V5M13.6667 13L17 9L13.6667 5M5.66667 9H16.3333" stroke="#FF1A43" strokeWidth="1.25" strokeLinecap="round" strokeLinejoin="round"/>
