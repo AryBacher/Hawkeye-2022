f95583d5f1a6893d9e83c7c582721d45471701e3
GuidoZyl <91797603+GuidoZyl@users.noreply.github.com>
2022-11-01 14:15:28 -0300

    Removed unnecessary files and minor changes

--- Back-End/server/database/dbhawkeye.sql
+++ Back-End/server/database/dbhawkeye.sql
@@ -1,15 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.1
+-- version 4.6.6
 -- https://www.phpmyadmin.net/
 --
--- Servidor: localhost
--- Tiempo de generación: 03-10-2022 a las 22:02:32
--- Versión del servidor: 8.0.17
--- Versión de PHP: 7.3.10
+-- Host: localhost
+-- Generation Time: Nov 01, 2022 at 04:34 PM
+-- Server version: 5.7.17-log
+-- PHP Version: 5.6.30
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -19,23 +17,13 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de datos: `dbhawkeye`
+-- Database: `dbhawkeye`
 --
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `estadisticas`
---
-
-CREATE TABLE `estadisticas` (
-  `idRegistro` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `usuarios`
+-- Table structure for table `usuarios`
 --
 
 CREATE TABLE `usuarios` (
@@ -48,18 +36,59 @@ CREATE TABLE `usuarios` (
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `videos`
+-- Table structure for table `videos`
 --
 
 CREATE TABLE `videos` (
   `idUsuario` int(11) NOT NULL,
-  `idVideo` int(11) NOT NULL,
-  `rutaVideo` varchar(100) NOT NULL,
+  `idCloudinary` varchar(124) NOT NULL,
+  `urlVideo` varchar(124) NOT NULL,
+  `urlMiniatura` varchar(124) NOT NULL,
   `titulo` varchar(50) NOT NULL,
   `rival` varchar(40) DEFAULT NULL,
-  `cancha` varchar(40) DEFAULT NULL
+  `tipo` varchar(20) NOT NULL,
+  `esFavorito` tinyint(1) NOT NULL,
+  `FechaPartido` date NOT NULL,
+  `arrayPiques` mediumtext NOT NULL,
+  `urlHeatmap` varchar(124) NOT NULL,
+  `velocidades` mediumtext NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-COMMIT;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `videos`
+--
+ALTER TABLE `videos`
+  ADD PRIMARY KEY (`idCloudinary`),
+  ADD KEY `FK` (`idUsuario`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `videos`
+--
+ALTER TABLE `videos`
+  ADD CONSTRAINT `FK` FOREIGN KEY (`idUsuario`) REFERENCES `usuarios` (`id`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
